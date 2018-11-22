7e63856de7a41dfc0ac43cf0c2a0c317a14f21c0
zhichengMLE <jason-dean@outlook.com>
2018-11-23 02:02:34 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Chart />
+  </div>
+</template>
+
+<script>
+import Chart from './components/Chart.component.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Chart
+  },
+}
+</script>
